dc044ec22f1f175b506897b1ada3562515f1494d
Luis Carlos Eich <pelinche@gmail.com>
2021-01-21 13:43:32 -0300

    Classes created

---
+++ src/Email.java
@@ -0,0 +1,16 @@
+public class Email {
+    private String mailAddress;
+
+
+    public Email(String mailAddress) {
+        this.mailAddress = mailAddress;
+    }
+
+    public String getMailAddress() {
+        return mailAddress;
+    }
+
+    public void setMailAddress(String mailAddress) {
+        this.mailAddress = mailAddress;
+    }
+}
